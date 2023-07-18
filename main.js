e129714b143e4ad5b8c03cb80f412372842e61d5
Santiago Gorosito <santigrst2@gmail.com>
2023-07-17 21:09:27 -0300

    primer entrega

---
+++ main.js
@@ -0,0 +1,125 @@
+/*console.log ("¡MIRÁ MAMÁAA, ESTOY PROGRAMANDO!")
+
+let numeroMayor = 12;
+let numeroMenor = 99;   
+
+console.log ( numeroMayor + numeroMenor)
+
+let nombre =  prompt("Ingrese su nombre");
+
+console.log ("Bienvenide a nuestra primera web " + nombre)
+
+let numberFive = (5*99);
+
+console.log (numberFive)
+
+let edad = prompt("Cuantos años tienes?");
+
+console.log (edad)
+
+let restarCien = (100 - numberFive);
+
+console.log (restarCien)
+
+let edat = prompt("Por favor, ingresa tu edad:");*/
+
+//1
+
+/*   const colorRojo = "rojo"
+const colorVerde = "verde"
+const colorAmarillo = "amarillo"
+const preguntarColor = prompt("Ingrese un color se un semaforo")
+
+if (preguntarColor === colorVerde) {
+    alert("Puede avanzar")
+} else if(preguntarColor === "rojo" && "amarillo") {
+    alert("No puede avanzar")
+} else {
+    alert("Error: color de semafoto invalido")
+}
+
+console.log(preguntarColor)
+
+//2
+
+const vocal = "a"
+const noVocal = "n"
+const numero = "1"
+const preguntarVocal = prompt("Ingrese una vocal")
+
+if (preguntarVocal === vocal) {
+    alert("Es una vocal")
+} else if (preguntarVocal === "n"&& "1") {
+    alert("No es una vocal")
+}
+
+console.log(preguntarVocal)
+
+//3
+
+const noConsonante = "a"
+const consonante = "n"
+const numeros = "2130213"
+const preguntarConsonante = prompt("Ingrese una Consonante")
+
+if (preguntarConsonante === consonante) {
+    alert("Es una consonante")
+} else if (preguntarConsonante === "1" &&"a") {
+    alert("No es una consonante")
+}
+
+console.log (preguntarConsonante)
+
+//4
+
+const pasoTest = "si";
+const tieneMultasImpagas = "no";
+const pagoImpuestos = "no";
+const preguntarTest = prompt("Hiciste el test?")
+const preguntarMultasImpagas = prompt("Tenes multas impagas?")
+const preguntarImpuestos = prompt("Pagaste los impuestos?")
+
+if (preguntarTest === pasoTest && preguntarMultasImpagas === tieneMultasImpagas && preguntarImpuestos === "si") {
+    alert("Puede renovar")
+} else {
+    alert("No puede renovar")
+}
+
+console.log (preguntarTest)
+
+//5
+const numeroOne = "2"
+const numeroPar = prompt("Ingresa un numero");
+
+if (numeroPar === numeroOne) {
+    lert("El numero es par");
+} else {
+    alert("El numero es impar");
+}
+
+console.log(numeroPar)
+
+//6
+
+const numeroPositivo = "3"
+const numeroNegativo = "-5"
+const preguntarPositivoOno = prompt("Ingrese un numero positivo(3) o negativo(-5)")
+
+if (preguntarPositivoOno === numeroPositivo){
+    alert("Es positivo")
+} else if (preguntarPositivoOno === "-5") {
+    alrt("Es negativo")
+}
+
+console.log(preguntarPositivoOno)
+
+//BUCLE
+
+const valorInical = prompt("Ingrese un numero para sumar:")
+const valorSumar = prompt("Ingrese otro numero:")
+const resultado = valorInical + valorSumar 
+
+for (valorInical; valorInical + valorSumar; valorInical++){
+    console.log(resultado)
+}
+*/
